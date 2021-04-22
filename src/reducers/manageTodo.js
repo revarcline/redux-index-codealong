aac5e489d65d6f936e094fb6d9bf07a4084fce25
Alex Cline <rev.a.r.cline@gmail.com>
2021-04-22 18:50:19 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,9 +1,11 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
+export default function manageTodo(
+  state = {
+    todos: [],
+  },
+  action,
+) {
   switch (action.type) {
-    case 'ADD_TODO':
-
+    case "ADD_TODO":
       return { todos: state.todos.concat(action.payload.text) };
 
     default:
